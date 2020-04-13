9290035ea192bd5a28562835cab06f635d90ac12
Fredrik Simonsson <simson@thesimson.net>
2020-04-13 21:00:31 +0900

    Letter I

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,83 @@
 #![no_main]
 #![no_std]
 
-extern crate panic_halt;
 use panic_halt as _;
 
 use cortex_m_rt::entry;
+
+use microbit::hal::delay::Delay;
 use microbit::hal::prelude::*;
+
+use microbit::led;
+// // extern crate panic_halt;
+// // use panic_halt as _;
+
+// use microbit::display;
+// // use cortex_m_rt::entry;
+// use microbit::hal::prelude::*;
+// use microbit::display::image::GreyscaleImage;
+// use microbit::display::{self, Display, Frame, MicrobitDisplayTimer, MicrobitFrame};
+// use microbit::hal::lo_res_timer::{LoResTimer,FREQ_512HZ,FREQ_32768HZ};
+// use microbit::hal::nrf51;
+// use rtfm::app;
+
+// use cortex_m_rt::entry;
+
+// // use microbit::hal::delay::Delay;
+// // use microbit::hal::prelude::*;
+
+// use microbit::led;
 
 #[entry]
 fn main() -> ! {
-    // unsafe { ALLOCATOR.init(cortex_m_rt::heap_start() as usize, 2048 as usize) };
 
      if let Some(p) = microbit::Peripherals::take() {
-    //     /* Split GPIO pins */
-         let mut gpio = p.GPIO.split();
-    //     let mut delay = hal::delay::Delay::new(p.TIMER0);
-    //     let pxx = cortex_m::Peripherals::take().unwrap();
+        let gpio = p.GPIO.split();
+        let mut delay = Delay::new(p.TIMER0);
 
-        // Set row of LED matrix to permanent high
-        let _ = gpio.pin13.into_push_pull_output().set_high();
-
-        // Set 2 columns to output to control LED states
-        let mut led1 = gpio.pin4.into_push_pull_output();
-        let mut led2 = gpio.pin6.into_push_pull_output();
+        let mut  display = microbit::led::Display::new(
+            gpio.pin4.into_push_pull_output(),
+            gpio.pin5.into_push_pull_output(),
+            gpio.pin6.into_push_pull_output(),
+            gpio.pin7.into_push_pull_output(),
+            gpio.pin8.into_push_pull_output(),
+            gpio.pin9.into_push_pull_output(),
+            gpio.pin10.into_push_pull_output(),
+            gpio.pin11.into_push_pull_output(),
+            gpio.pin12.into_push_pull_output(),
+            gpio.pin13.into_push_pull_output(),
+            gpio.pin14.into_push_pull_output(),
+            gpio.pin15.into_push_pull_output());
 
         // Configure button GPIOs as inputs
         let button_a = gpio.pin17.into_floating_input();
         let button_b = gpio.pin26.into_floating_input();
-
+        // led::display.led::display_pre_u32(&mut delay, bitmaps::img::square_image , 300);
+        // led::display.led::display_pre_u32(&mut delay, bitmaps::img::square_small_image, 300);
+        // led::display.led::display_pre_u32(&mut delay, bitmaps::img::dot33 , 300);
+        // led::display.led::display_pre_u32(&mut delay, bitmaps::img::diamond_small_image , 300);
+        // led::display.led::display_pre_u32(&mut delay, bitmaps::img::diamond_image , 300);
+        #[allow(non_snake_case)]
+        let letter_I = [
+            [0, 1, 1, 1, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 1, 1, 1, 0],
+        ];
         loop {
             if let Ok(true) = button_a.is_high() {
-                let _ = led1.set_high();
+                display.display(&mut delay, letter_I, 1000);
+                
+                
             } else {
-                let _ = led1.set_low();
+                
             }
-
-            if let Ok(true) = button_b.is_high() {
-                let _ = led2.set_high();
-            } else {
-                let _ = led2.set_low();
-            }
+            // if let Ok(true) = button_b.is_high() {
+            //     let _ = led2.set_high();
+            // } else {
+            //     let _ = led2.set_low();
+            // }
         }
     }
 
